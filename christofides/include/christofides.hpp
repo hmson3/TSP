b2bc9e0e6320229759b1d59c5ff477480698de5b
Son_hyeongmin <shm0924@unist.ac.kr>
2025-05-16 22:07:13 +0900

    christofides

--- christofides/include/christofides.hpp
+++ christofides/include/christofides.hpp
@@ -5,3 +5,6 @@ int christofides(const std::vector<std::vector<int>>& dist);
 std::vector<std::pair<int, int>> compute_mst(const std::vector<std::vector<int>>& dist, int n);
 std::vector<int> find_odd_nodes(const std::vector<std::pair<int, int>>& edges, int n);
 std::vector<std::pair<int, int>> greedy_matching(const std::vector<int>& odd_nodes, const std::vector<std::vector<int>>& dist);
+std::vector<std::vector<int>> graph_make(const std::vector<std::pair<int, int>>& mst, const std::vector<std::pair<int, int>>& matching, int n);
+std::vector<int> find_eulerian_tour(const std::vector<std::vector<int>>& graph);
+std::vector<int> hamiltonian_cycle(const std::vector<int>& tour);
--- christofides/src/christofides.cpp
+++ christofides/src/christofides.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <queue>
 #include <algorithm>
+#include <set>
+#include <stack>
 
 using namespace std;
 
@@ -28,10 +30,26 @@ int christofides(const vector<vector<int>>& dist) {
 
     auto matchings = greedy_matching(odd_nodes, dist);
     for (auto [u, v] : matchings) {
-        cout << u << " - " << v << " (dist = " << dist[u][v] << ")" << endl;
+        cout << u << " - " << v << " (dist = " << dist[u][v] << ")" << '\n';
     }
+    auto graph = graph_make(mst,matchings, n);
+    for (int u = 0; u < n; u++) {
+        cout << u << " : ";
+        for (int v : graph[u]) cout << v << " ";
+        cout << '\n';
+    }
+    auto tour = find_eulerian_tour(graph);
 
-    return -1; 
+    cout << "Eulerian Tour: ";
+    for (int v : tour) cout << v << " ";
+    cout << '\n';
+
+    auto cycle = hamiltonian_cycle(tour);
+    
+    int ret = 0;
+    for(int i = 0; i < n; i++) ret += dist[cycle[i]][cycle[i+1]];
+
+    return ret; 
 }
 
 
@@ -72,7 +90,7 @@ vector<int> find_odd_nodes(const vector<pair<int, int>>& edges, int n) {
     }
 
     vector<int> odd_nodes;
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; i++) {
         if (degree[i] % 2 == 1)
             odd_nodes.push_back(i);
     }
@@ -110,5 +128,76 @@ vector<pair<int, int>> greedy_matching(const vector<int>& odd_nodes, const vecto
         nn += 2;
     }
 
+    return ret;
+}
+
+vector<vector<int>> graph_make(const vector<pair<int, int>>& mst, const vector<pair<int, int>>& matching, int n) {
+    vector<vector<int>> ret(n);
+
+    // MST 간선 추가
+    for (auto [u, v] : mst) {
+        ret[u].push_back(v);
+        ret[v].push_back(u);
+    }
+
+    // Matching 간선 추가
+    for (auto [u, v] : matching) {
+        ret[u].push_back(v);
+        ret[v].push_back(u);
+    }
+
+    return ret;
+}
+// Hierholzer’s Algorithm
+// 1. 정점에서 시작
+// 2. 갈 수 있는 간선을 따라 DFS처럼 탐색하며 경로 저장
+// 3. 더 이상 갈 데가 없으면 백트래킹하면서 경로를 이어붙임
+// [0,1,2,0] + [1,3,1] => [0,1,3,1,2,0]
+// 4. 모든 간선 쓸때까지 반복
+vector<int> find_eulerian_tour(const vector<vector<int>>& graph) {
+    int n = graph.size();
+    vector<int> tour;
+    vector<multiset<int>> adj(n);
+    stack<int> s;
+    // 간선이 중복가능하고 없애야 하기 때문에 multiset 사용
+    // set 사용시 간선 중복 불가, 사용하지 않을 시 간선 삭제에 O(n) 걸림
+    for (int u = 0; u < n; u++)
+        for (int v : graph[u])
+            adj[u].insert(v);
+    
+    s.push(0);
+    
+    while(!s.empty()) {
+        int u = s.top();
+        if(adj[u].empty()) {
+            tour.push_back(u);
+            s.pop();
+        }
+        else {
+            auto it = adj[u].begin();
+            int v = *it;
+            adj[u].erase(it);
+            adj[v].erase(adj[v].find(u));
+            s.push(v);
+        }
+    }
+
+    reverse(tour.begin(),tour.end());
+
+    return tour;
+}
+
+vector<int> hamiltonian_cycle(const vector<int>& tour) {
+    vector<bool> visited(tour.size(), false);
+    vector<int> ret;
+
+    for (int node : tour) {
+        if (!visited[node]) {
+            visited[node] = true;
+            ret.push_back(node);
+        }
+    }
+
+    ret.push_back(ret[0]);
     return ret;
 }
